910e48c1c45b5f30b72a48a6780807b44aae0b63
tpdbs222 <tpdbs222@gmail.com>
2022-10-18 11:23:48 +0900

    TodoApp TodoForm RodoList ~~

---
+++ src/TodoApp_MSY1126.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+const TodoApp_MSY1126 = () => {
+    return <div>
+        <TodoForm />
+        <TodoList todos = {[]} />
+
+        </div>;
+};
+
+export default TodoApp_MSY1126;
---
+++ src/TodoApp_MSY1126.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import TodoApp_MSY1126 from './TodoApp_MSY1126';
+
+describe('<TodoApp_MSY1126/>', () => {
+    it ('renders TodoForm TodoList', () => {
+        // getByTestId : list .....확인용
+        const {getByText, getByTestId } = render(<TodoApp_MSY1126/>);
+        getByText('등록');  // TodoForm 화면에 있는지 확인
+        getByTestId('TodoList');  // TodoList 화면에 있는지 확인
+    });
+});
