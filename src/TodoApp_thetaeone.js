b0a5e9c8ae2ae0c46a68a1e581d5db31c7aaa7f0
taeone <taewon1554@gmail.com>
2022-10-18 12:17:41 +0900

    TodoApp onToggle event test OK

---
+++ src/TodoApp_thetaeone.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useState, useRef } from 'react';
+import TodoList from './TodoList';
+import TodoForm from "./TodoForm";
+const TodoApp_thetaeone = () => {
+// TodoApp에서 필요한 TodoForm과 TodoList 를 가져옴.
+    // Todos에 들어갈 변수 설정
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'TDD 배우기',
+            done: false,
+        },
+        {
+            id: 2,
+            text: 'react-testing-library',
+            done: true,
+        },
+    ]);
+    // id 변수값을 만들기 위함
+    // 실제 서버에서 받아올때는 axios get 사용
+    // 충돌방지를 위해 3부터 사용
+    const nextId = useRef(3);
+    // TodoForm에서 사용할 함수 onInsert
+    const onInsert = useCallback( text => {
+        setTodos(
+            todos.concat({
+                id: nextId.current,
+                text,
+                done: false
+            })
+        );
+        nextId.current += 1;
+    },[todos]);
+    const onToggle = useCallback(id => {
+        setTodos(
+            todos.map(todo => todo.id === id ? {...todo, done: !todo.done}: todo)
+        );
+    },[todos]);
+    return (
+        <div>
+            <TodoForm onInsert={onInsert}/>
+            <TodoList todos={todos} onToggle={onToggle}/>
+        </div>
+        );
+};
+export default TodoApp_thetaeone;
---
+++ src/TodoApp_thetaeone.test.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TodoApp_thetaeone from "./TodoApp_thetaeone";
+
+describe('<TodoApp_thetaeone />', () =>{
+    it('renders TodoForm TodoList', ()=>{
+        const {getByText, getByTestId} =render(<TodoApp_thetaeone />);
+        getByText('등록'); // TodoForm 화면에 있는지 확인
+        getByTestId('TodoList'); // TodoList 화면에 있는지 확인
+    });
+    it('renders two defaults todos', () =>{
+        const {getByText} = render(<TodoApp_thetaeone />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
+    it('creates new todo', () => {
+        const { getByPlaceholderText, getByText } = render(<TodoApp_thetaeone />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요'),
+            { target: { value: "새 항목 추가하기" } }
+        );
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가하기');
+    });
+    it('toggles todo', () =>{
+        const {getByText} = render(<TodoApp_thetaeone />);
+        const todoText = getByText('TDD 배우기');
+        expect(todoText).not.toHaveStyle('text-decoration: line-through');
+        fireEvent.click(todoText);
+        expect(todoText).toHaveStyle('text-decoration: line-through');
+    });
+});
