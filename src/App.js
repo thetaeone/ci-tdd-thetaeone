61b402d5443df19e8a6412a1b21e26cfddc60811
taeone <taewon1554@gmail.com>
2022-10-18 12:42:13 +0900

    TodoApp test OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './TodoForm';
+import TodoApp_thetaeone from './TodoApp_thetaeone';
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoApp_thetaeone />
+      Learn React
     </div>
   );
 }
